127f7d7e3ad5a29adff58d15490da2b7b8c954a8
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-25 11:46:12 -0300

    arquivo inicial do spring

---
+++ src/main/java/com/example/Spotifyyy/SpotifyyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spotifyyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyyyApplication.class, args);
+	}
+
+}
