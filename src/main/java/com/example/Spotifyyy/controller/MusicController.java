eb77d2a94874bc46e7d940d9dd1b5b6dd01edfcd
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-25 11:45:27 -0300

    Criando a classe controller

---
+++ src/main/java/com/example/Spotifyyy/controller/MusicController.java
@@ -0,0 +1,9 @@
+package com.example.Spotifyyy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("music")
+public class MusicController {
+}
