70c6cccb345f21ea4b5c886d76d3204ee29c32d4
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-25 17:05:01 -0300

    configurando a conexao com db

--- src/main/java/com/example/Spotifyyy/music/Music.java
+++ src/main/java/com/example/Spotifyyy/music/Music.java
@@ -1,13 +1,14 @@
 package com.example.Spotifyyy.music;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Table(name = "musics")
 @Entity(name = "musics")
 public class Music {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String author;
     private String nameMusic;
 }
--- src/main/resources/db/migration/V1__create-table-musics.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE musics (
-    id SERIAL PRIMARY KEY,
-    author TEXT NOT NULL,
-    nameMusic TEXT NOT NULL
-);
