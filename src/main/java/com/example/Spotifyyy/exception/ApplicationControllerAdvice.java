d5b4ae7c9c9c5c45307468a0fc9070f75cc67490
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-28 16:43:43 -0300

    criando tratemento de exeptions

---
+++ src/main/java/com/example/Spotifyyy/exception/ApplicationControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.example.Spotifyyy.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler {
+
+    private final HttpStatus notFound = HttpStatus.NOT_FOUND;
+    private final HttpStatus internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<DefaultError> handleNoSuchElementException(NoSuchElementException ex) {
+
+        return ResponseEntity.status(notFound)
+                .body(new DefaultError(notFound.value(), "Resource not found"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<DefaultError> handleException(Exception ex) {
+        return ResponseEntity.status(internalServerError)
+                .body(new DefaultError(internalServerError.value(), "Internal Server Error"));
+    }
+}
